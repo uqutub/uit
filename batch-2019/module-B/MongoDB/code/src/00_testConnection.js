05a87005d2ab2c4670c7fd8e011224916eebd08c
Yousuf Qutubuddin <usuf52@gmail.com>
2019-12-14 12:12:29 +0500

    added css code

--- batch-2019/module-B/MongoDB/code/src/00_testConnection.js
+++ batch-2019/module-B/MongoDB/code/src/00_testConnection.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://admin:admin123@ds119652.mlab.com:19652/demo-uit-class')
+
+// mongoose.connect('mongodb://admin:admin123@ds119652.mlab.com:19652/demo-uit-class')
+mongoose.connect('mongodb://admin:admin123@ds049104.mlab.com:49104/uit-moduleb')
 
 mongoose.connection
 	.once('open', () => {
 		console.log('Yahooo! Connection is Established.');
+
+		const UserSchema = new Schema({
+			name: String
+		});
+		
+		const User = mongoose.model('user', UserSchema);
+
+
+		const joe = new User({ name: 'Usuf' });
+		console.log(joe.isNew);
+		joe.save()
+		.then((u) => {
+			console.log('Saved ', u)
+			console.log(joe.isNew);
+		})
+		.catch((err) => console.log('Err ', err));
+
 	})
 	.on('error', (err) => {
 		console.log('Err: ', err);
